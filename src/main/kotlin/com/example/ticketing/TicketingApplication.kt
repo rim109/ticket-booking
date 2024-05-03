2bce135dd81a1214eaeb6e76f47b42bd1b20e175
rim109 <fla7722@naver.com>
2024-05-03 23:27:31 +0900

    Add: 어노테이션 추가

--- src/main/kotlin/com/example/ticketing/TicketingApplication.kt
+++ src/main/kotlin/com/example/ticketing/TicketingApplication.kt
@@ -3,7 +3,9 @@ package com.example.ticketing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@EnableJpaRepositories
 @EnableJpaAuditing
 @SpringBootApplication
 class TicketingApplication
