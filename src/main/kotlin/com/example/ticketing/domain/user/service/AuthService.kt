32a29f3db08bdbcfdead1e1f1cdd1ed1f245e443
rim109 <fla7722@naver.com>
2024-06-04 04:34:41 +0900

    style: 간단한 기능 설명을 위해 주석 작성
    #8

--- src/main/kotlin/com/example/ticketing/domain/user/service/AuthService.kt
+++ src/main/kotlin/com/example/ticketing/domain/user/service/AuthService.kt
@@ -8,9 +8,12 @@ import com.example.ticketing.domain.user.dto.response.UserResponse
 
 interface AuthService {
 
+    // 로그인
     fun login(req: LoginRequest): LoginResponse
 
+    // 회원가입
     fun signup(req: SignupRequest): UserResponse
 
+    // 로그아웃 - 아직 안해본 것으로 차차 추가할 예정 임시로 틀만 작성함!
     fun logout()
 }
--- src/main/kotlin/com/example/ticketing/domain/user/service/UserService.kt
+++ src/main/kotlin/com/example/ticketing/domain/user/service/UserService.kt
@@ -4,9 +4,12 @@ import com.example.ticketing.domain.user.dto.response.UserResponse
 
 interface UserService {
 
+    // 프로필 조회
     fun getUser(userId: Long): UserResponse
 
+    // 프로필 수정
     fun updateUser(userId: Long): UserResponse
 
+    // 탈퇴하기
     fun deleteUser(userId: Long)
 }
