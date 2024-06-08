cf7737e4a89d9363a832ccf702f5323d3a7c0a56
rim109 <fla7722@naver.com>
2024-06-09 01:47:11 +0900

    feat: 로그인, 회원가입 로직 구현
    #8

--- src/main/kotlin/com/example/ticketing/domain/user/controller/AuthController.kt
+++ src/main/kotlin/com/example/ticketing/domain/user/controller/AuthController.kt
@@ -6,24 +6,28 @@ import com.example.ticketing.domain.user.dto.request.LoginRequest
 import com.example.ticketing.domain.user.dto.request.SignupRequest
 import com.example.ticketing.domain.user.dto.response.LoginResponse
 import com.example.ticketing.domain.user.dto.response.UserResponse
+import com.example.ticketing.domain.user.service.AuthService
+import com.example.ticketing.domain.user.service.UserService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/auth")
-class AuthController {
+class AuthController(
+    private val authService: AuthService
+) {
 
     @PostMapping("/login")
     fun login(
         @RequestBody req: LoginRequest
     ): ResponseEntity<LoginResponse> {
-        return ResponseEntity.status(HttpStatus.OK).build()
+        return ResponseEntity.status(HttpStatus.OK).body(authService.login(req))
     }
 
     @PostMapping("/signup")
     fun signup(
         @RequestBody req: SignupRequest
     ): ResponseEntity<UserResponse> {
-        return ResponseEntity.status(HttpStatus.OK).build()
+        return ResponseEntity.status(HttpStatus.OK).body(authService.signup(req))
     }
 
     @GetMapping("/logout")
