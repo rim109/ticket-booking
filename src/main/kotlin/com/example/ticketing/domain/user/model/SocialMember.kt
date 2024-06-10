c1d04a5affc17a174c4b9209f20baec7d65adc80
rim109 <fla7722@naver.com>
2024-06-11 01:30:31 +0900

    feat: 소셜로그인 엔티티 작성
    #10

---
+++ src/main/kotlin/com/example/ticketing/domain/user/model/SocialMember.kt
@@ -0,0 +1,37 @@
+package com.example.ticketing.domain.user.model
+
+import com.example.ticketing.common.model.BaseTimeEntity
+import com.example.ticketing.common.oauth.provider.OAuth2Provider
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "social_member")
+class SocialMember(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Enumerated(EnumType.STRING)
+    val provider: OAuth2Provider,
+    val providerId: String,
+    val nickname: String
+
+) : BaseTimeEntity() {
+    companion object {
+        fun ofKakao(id: Long, nickname: String): SocialMember {
+            return SocialMember(
+                provider = OAuth2Provider.KAKAO,
+                providerId = id.toString(),
+                nickname = nickname
+            )
+        }
+
+        fun ofGoogle(id: Long, nickname: String): SocialMember {
+            return SocialMember(
+                provider = OAuth2Provider.GOOGLE,
+                providerId = id.toString(),
+                nickname = nickname
+            )
+        }
+    }
+}
