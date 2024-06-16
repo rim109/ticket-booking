2a65831cb36e78f7307d504a7091c282bd7ee1ce
rim109 <fla7722@naver.com>
2024-06-17 00:27:43 +0900

    feat: 오어스 로그인 관료 로직 작성 #10

---
+++ src/main/kotlin/com/example/ticketing/domain/user/dto/request/OAuth2UserInfo.kt
@@ -0,0 +1,11 @@
+package com.example.ticketing.domain.user.dto.request
+
+import com.example.ticketing.common.oauth.provider.OAuth2Provider
+
+open class OAuth2UserInfo (
+    val provider: OAuth2Provider,
+    val id: String,
+    val nickname: String,
+    val email: String,
+    val profileImage: String?
+)
---
+++ src/main/kotlin/com/example/ticketing/domain/user/service/SocialUserService.kt
@@ -0,0 +1,26 @@
+package com.example.ticketing.domain.user.service
+
+import com.example.ticketing.common.oauth.provider.OAuth2Provider
+import com.example.ticketing.domain.user.dto.request.OAuth2UserInfo
+import com.example.ticketing.domain.user.model.SocialMember
+import com.example.ticketing.domain.user.repository.SocialUserRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class SocialUserService(
+    private val socialUserRepository: SocialUserRepository
+) {
+    @Transactional
+    fun registerIfAbsent(oAuth2UserInfo: OAuth2UserInfo): SocialMember {
+        return if (!socialUserRepository.existsByProviderAndProviderId(oAuth2UserInfo.provider, oAuth2UserInfo.id)) {
+            val socialUser = when(oAuth2UserInfo.provider){
+                OAuth2Provider.KAKAO -> SocialMember.ofKakao(oAuth2UserInfo.id.toLong(), oAuth2UserInfo.email, oAuth2UserInfo.nickname)
+                OAuth2Provider.GOOGLE -> SocialMember.ofGoogle(oAuth2UserInfo.id.toLong(), oAuth2UserInfo.email, oAuth2UserInfo.nickname)
+            }
+            socialUserRepository.save(socialUser)
+        } else {
+            socialUserRepository.findByProviderAndProviderId(oAuth2UserInfo.provider, oAuth2UserInfo.id)
+        }
+    }
+}
