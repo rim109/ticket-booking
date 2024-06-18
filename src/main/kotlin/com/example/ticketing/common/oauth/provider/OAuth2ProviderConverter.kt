210209cc8bd8b5bdf5e3b4dde227365ecc3507ae
rim109 <fla7722@naver.com>
2024-06-18 22:27:07 +0900

    feat: 오어스 Converter와 WebConfig 작성
    #10

--- src/main/kotlin/com/example/ticketing/common/oauth/provider/OAuth2ProviderConverter.kt
+++ src/main/kotlin/com/example/ticketing/common/oauth/provider/OAuth2ProviderConverter.kt
@@ -1,8 +1,14 @@
 package com.example.ticketing.common.oauth.provider
 
 import org.springframework.core.convert.converter.Converter
+import java.security.InvalidParameterException
+
 class OAuth2ProviderConverter: Converter <String, OAuth2Provider>{
     override fun convert(source: String): OAuth2Provider {
-        TODO("Not yet implemented")
+        return kotlin.runCatching {
+            OAuth2Provider.valueOf(source.uppercase())
+        }.getOrElse {
+            throw InvalidParameterException("$it")
+        }
     }
 }
