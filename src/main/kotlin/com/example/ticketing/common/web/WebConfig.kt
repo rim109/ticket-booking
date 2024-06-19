8e0264751258210d7e00743dfc8e879c909bc6a2
rim109 <fla7722@naver.com>
2024-06-19 22:25:48 +0900

    add: cors 관련 로직 추가
    #10

--- src/main/kotlin/com/example/ticketing/common/web/WebConfig.kt
+++ src/main/kotlin/com/example/ticketing/common/web/WebConfig.kt
@@ -3,11 +3,19 @@ package com.example.ticketing.common.web
 import com.example.ticketing.common.oauth.provider.OAuth2ProviderConverter
 import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addFormatters(registry: FormatterRegistry) {
         registry.addConverter(OAuth2ProviderConverter())
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+            .allowedOrigins("http://localhost:8080/**") // 이 출처로부터의 요청만 허용
+            .allowedMethods("*") // 모든 HTTP 메소드 허용
+            .allowCredentials(true) // 쿠키를 포함한 요청 허용
     }
 }
